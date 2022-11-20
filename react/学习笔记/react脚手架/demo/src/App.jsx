231e2bb3cd38c8dab72a60dc209f0459abad9f62
coderschao <coderschao@foxmail.com>
2022-11-20 17:11:14 +0800

    购物车案例

---
+++ react/学习笔记/react脚手架/demo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ShoppingCart from "./components/ShoppingCart";
+class App extends React.Component {
+  render() {
+    return (
+      <ShoppingCart />
+    )
+  }
+}
+
+export default App
---
+++ react/学习笔记/react脚手架/demo/src/data/books.js
@@ -0,0 +1,26 @@
+export const books = [
+  {
+    id:1,
+    name:"高级JavaScript",
+    price: 322,
+    count:1
+  },
+  {
+    id: 2,
+    name: "CSS权威指南",
+    price: 116,
+    count: 1
+  },
+  {
+    id: 3,
+    name: "你不知道的JavaScript",
+    price: 89,
+    count: 1
+  },
+  {
+    id: 4,
+    name: "HTTP权威指南",
+    price: 96,
+    count: 1
+  }
+]
